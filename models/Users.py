27a0c30abb6787127235cfbff9ec9c6f58a20341
eslam eltaieb <eslameltaieb74@gmail.com>
2025-02-21 13:34:07 +0100

    user schema created

---
+++ models/Users.py
@@ -0,0 +1,17 @@
+from config.db_connect import Base
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Integer , String , Boolean
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class DbUser(Base):
+    __tablename__ ='users'
+    id = Column(Integer,primary_key=True, index= True)
+    user_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    about = Column(String)
+    avatar = Column(String, nullable=True)
+    phone_number = Column(String, nullable=True)
+    
