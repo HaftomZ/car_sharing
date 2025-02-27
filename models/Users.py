7eee1f07984352f5d2670bda04f1355a061043e3
HaftomZ <122086208+HaftomZ@users.noreply.github.com>
2025-02-27 05:05:00 +0100

    Update Users.py
    Average_rating column was repeated in users table. I corrected it.

--- models/Users.py
+++ models/Users.py
@@ -22,6 +22,6 @@ class DbUser(Base):
     left_reviews = relationship("DbReview", back_populates="creator", foreign_keys="[DbReview.creator_id]")
     received_reviews = relationship("DbReview", back_populates="receiver", foreign_keys="[DbReview.receiver_id]",
                                     cascade="all, delete-orphan")
-    average_rating = Column(Float)
+   
     trip = relationship("DbTrip", back_populates="user", cascade="all, delete-orphan")
 
